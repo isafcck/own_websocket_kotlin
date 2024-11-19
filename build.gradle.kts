5898bad290ed646a88c04f19f07cd17decc7aa3a
isafcck <150579399+isafcck@users.noreply.github.com>
2024-11-19 09:08:06 +0530

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,1 +1,24 @@
 
+plugins {
+    kotlin("jvm") version "1.8.21"
+    application
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+    implementation("org.java-websocket:Java-WebSocket:1.5.2")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.7.1")
+    testImplementation("org.junit.jupiter:junit-jupiter-engine:5.7.1")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+application {
+    mainClass.set("com.websocket.client.WebSocketClientKt")
+}
