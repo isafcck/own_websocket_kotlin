28ec4e21e379bd285ef0cd087f47d51df7ac3843
isafcck <150579399+isafcck@users.noreply.github.com>
2024-11-19 09:48:42 +0530

    Update Main.kt

--- src/main/kotlin/client/Main.kt
+++ src/main/kotlin/client/Main.kt
@@ -1,1 +1,18 @@
+package com.websocket.client
+import java.net.URI
 
+fun main(){
+  val serverURI =URI("ws://example.com.socket")
+  
+  val websocketClient= WebSocketExample(serverURI)
+
+  println("connecting to server")
+ websocketClient.connectToServer()
+ 
+  println("waiting for messages")
+  Thread.sleep(1000)
+  
+  websocketClient.closeConnection()
+  Thread.sleep(2000)
+  
+}
