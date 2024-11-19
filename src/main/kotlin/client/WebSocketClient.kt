e8e2b8328f128f4a2e6293380479deef2528ee3e
isafcck <150579399+isafcck@users.noreply.github.com>
2024-11-19 09:35:08 +0530

    Update WebSocketClient.kt

--- src/main/kotlin/client/WebSocketClient.kt
+++ src/main/kotlin/client/WebSocketClient.kt
@@ -1,1 +1,35 @@
+package com.websocket.client
 
+import org.java-websocket.client.WebSocketClient
+import org.java-websocket.handshake.ServerHandshake
+import java.net.URI
+
+class WebSocketClientExample(serverURI:URI):WebSocketClient(serverURI){
+  
+  override fun  onOpen(handshake: ServerHandshake?){
+    println("opened connection")
+    send("hello server")
+    
+  }
+  override fun onMessage(message:String?){
+    println(" message $message")
+    
+  }
+
+  override fun onClose(reason:String?,code:Int,remote:Boolean?){
+    println("connection closed with $reason with code $code")
+    
+  }
+  override fun onError(ex:Exception?){
+    println("error occured ${ex?.message}")
+  }
+  fun connectToServer(){
+    connect()
+  }
+  
+  fun closeConnection(){
+    close()
+    
+    
+  }
+  
