e3f4d9d078b34240c77b6806407ab41b7bc7fbe7
isafcck <150579399+isafcck@users.noreply.github.com>
2024-11-19 09:50:27 +0530

    Update Logger.kt

--- src/main/kotlin/utils/Logger.kt
+++ src/main/kotlin/utils/Logger.kt
@@ -1,1 +1,34 @@
+package com.websocket.client.util
+
+import java.util.logging.ConsoleHandler
+import java.util.logging.Level
+import java.util.logging.Logger
+
+object LoggerUtil {
+
+    private val logger: Logger = Logger.getLogger(LoggerUtil::class.java.name)
+
+    init {
+        val consoleHandler = ConsoleHandler()
+        consoleHandler.level = Level.ALL
+        logger.addHandler(consoleHandler)
+        logger.level = Level.ALL
+    }
+
+    fun info(message: String) {
+        logger.info(message)
+    }
+
+    fun warn(message: String) {
+        logger.warning(message)
+    }
+
+    fun error(message: String) {
+        logger.severe(message)
+    }
+
+    fun debug(message: String) {
+        logger.fine(message)
+    }
+}
 
